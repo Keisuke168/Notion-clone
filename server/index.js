7f74be68312bd6ca58aa75eecf09aa8add0591d9
Keisuke168 <k.yoshimi313@ezweb.ne.jp>
2022-11-06 16:26:46 +0900

    refactaring

--- server/index.js
+++ server/index.js
@@ -1,43 +1,21 @@
-const express = require("express")
+const express = require("express");
 const mongoose = require("mongoose");
-const CryptoJS = require("crypto-js");
-const JWT = require("jsonwebtoken");
-const User = require("./src/v1/models/user");
 const app = express();
 const PORT = 5000;
 require("dotenv").config();
 
 app.use(express.json());
+app.use("/api/v1", require("./src/v1/routes/auth"));
 
 //DB connection
 try {
-    mongoose.connect(process.env.MONGODB_URL);
-    console.log("connecting to DB");
-} catch(error) {
-    console.log(error);
+  mongoose.connect(process.env.MONGODB_URL);
+  console.log("connecting to DB");
+} catch (error) {
+  console.log(error);
 }
-//register new user API
-app.post("/register", async (req, res) => {
-    const password = req.body.password;
-
-    try {
-        // encrypt password
-        req.body.password = CryptoJS.AES.encrypt(password, process.env.SECRET_KEY);
-        // create new user
-        const user = await User.create(req.body);
-        // JWT
-        const token = JWT.sign({ id: user._id}, process.env.TOKEN_SECRET_KEY, {
-            expiresIn: "24h",
-        });
-        return res.status(200).json({ user, token }); 
-    }catch(error){
-        return res.status(500).json( error );
-    }
-});
-
-
 //user login API
 
-app.listen(PORT, ()=> {
-    console.log("starting local server...")
+app.listen(PORT, () => {
+  console.log("starting local server...");
 });
---
+++ server/src/v1/routes/auth.js
@@ -0,0 +1,32 @@
+const router = require("express").Router();
+const { body } = require("express-validator");
+require("dotenv").config();
+
+const User = require("../models/user");
+const validation = require("../handlers/validation");
+const userController = require("../controllers/user");
+
+//register new user API
+router.post(
+  "/register",
+  body("username")
+    .isLength({ min: 8 })
+    .withMessage("ユーザー名は８文字以上である必要があります"),
+  body("password")
+    .isLength({ min: 8 })
+    .withMessage("パスワードは８文字以上である必要があります"),
+  body("confirmPassword")
+    .isLength({ min: 8 })
+    .withMessage("確認用パスワードは８文字以上である必要があります"),
+  body("username").custom((value) => {
+    return User.findOne({ username: value }).then((user) => {
+      if (user) {
+        return Promise.reject("このユーザは既に使われています。");
+      }
+    });
+  }),
+  validation.validate,
+  userController.register
+);
+
+module.exports = router;
